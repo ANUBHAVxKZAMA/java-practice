8d3ac29dec30a8be011f418103e4def7664a5d99
Anubhav Srivastav <anubhavsrivastav12398@gmail.com>
2026-01-07 12:39:58 +0530

    Your message

--- src/basics/Add.java
+++ src/basics/Add.java
@@ -7,7 +7,7 @@ public class Add{
         double x=sc.nextDouble();
         System.out.println("ENTER A NUMBER:");
         double y=sc.nextDouble();
-        System.out.println("SUM OF THE GIVEN NUMBERS="+x+y);
+        System.out.println("SUM OF THE GIVEN NUMBERS="+(x+y));
 	}
 }
 
--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -1,6 +1,5 @@
 package basics;
-import java.util.*;
-public class HelloWorld {
+class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Java setup is working.");
     }
